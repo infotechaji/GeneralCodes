4aeb34372f78be04a3811c384b7bd2a3b5cb4203
Ajithkumar Mariyappan <infotechaji@gmail.com>
2020-07-13 12:12:23 +0530

    Django leaning

--- Django/Learnings/allprojects/testM/calc/urls.py
+++ Django/Learnings/allprojects/testM/calc/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views # the views file is imported here
 
 urlpatterns = [
-    path('test', views.home,name='Index'),
+    path('', views.home,name='Index'),
+    path('test-template', views.home_template,name='Template-Index'),
+    path('test-template-dy', views.home_template_dynamic,name='Template-Index-DYM'),
+    path('add', views.add,name='add'),
 ]
--- Django/Learnings/allprojects/testM/calc/views.py
+++ Django/Learnings/allprojects/testM/calc/views.py
@@ -7,3 +7,17 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello world !")
+
+
+def home_template(request):
+    return render(request, 'home_template.html')
+
+
+def home_template_dynamic(request):
+    return render(request, 'home_template_dy.html', {'name': 'Ajithkumar-M'})
+
+def add(request):
+    val1=int(request.GET['num1'])
+    val2=int(request.GET['num2'])
+    data=val1+val2
+    return render(request, 'result.html',{'data':data})
